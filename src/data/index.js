10ca341a95655db6b621ccf09f111a2f13be7bfc
Adrien Navratil <adrien1975@live.fr>
2021-02-01 02:09:13 +0100

    Using environment variables for firebase config

--- src/data/index.js
+++ src/data/index.js
@@ -6,5 +6,12 @@ import 'firebase/auth';
 let app;
 
 export function init() {
-    app = firebase.initializeApp(require('../../firebase.json'));
+    app = firebase.initializeApp({
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID
+    });
 }
