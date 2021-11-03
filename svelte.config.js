51d21db86ed7ab87118d1e2c23f6ec206d688871
Adrien Navratil <adrien1975@live.fr>
2021-11-03 18:24:42 +0100

    Replaced tabs by spaces

--- svelte.config.js
+++ svelte.config.js
@@ -3,33 +3,33 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		target: '#app',
-		adapter: adapter({
-			precompress: true
-		}),
-		vite: () => ({
-			resolve: {
-				alias: {
-					// The 'replace' values in the preprocess options should work when using /components, but for
-					// some reason it doesn't work on the first build when importing Svelte components.
-					// I'm using this instead until the issue is fixed.
-					'$components': '/src/components'
-				}
-			}
-		})
-	},
-	preprocess: preprocess({
-		replace: [
-			[/import '\//g, "import '/src/"],
-			[/from '\//g, "from '/src/"]
-		],
-		sourceMap: true,
-		scss: {
-			renderSync: true,
-			includePaths: ['src/styles']
-		}
-	})
+    kit: {
+        target: '#app',
+        adapter: adapter({
+            precompress: true
+        }),
+        vite: () => ({
+            resolve: {
+                alias: {
+                    // The 'replace' values in the preprocess options should work when using /components, but for
+                    // some reason it doesn't work on the first build when importing Svelte components.
+                    // I'm using this instead until the issue is fixed.
+                    '$components': '/src/components'
+                }
+            }
+        })
+    },
+    preprocess: preprocess({
+        replace: [
+            [/import '\//g, "import '/src/"],
+            [/from '\//g, "from '/src/"]
+        ],
+        sourceMap: true,
+        scss: {
+            renderSync: true,
+            includePaths: ['src/styles']
+        }
+    })
 };
 
 export default config;
